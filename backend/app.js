eb9c82f807542e65053e8669fb75cb5b70118579
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-01-22 19:52:59 -0500

    Set up server for backend

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ index: "This is the index page" });
+});
+
+module.exports = app;
