123bdd095ffe1c721beea2601f7dcd960553d80c
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-01-29 14:03:35 -0500

    feat(uploads): allow only audio files

--- backend/Controllers/audioFileController.js
+++ backend/Controllers/audioFileController.js
@@ -3,17 +3,20 @@ const audio = express.Router();
 const upload = require("../Upload/audioUpload");
 
 audio.post("/upload", upload.single("file"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: "No File Uploaded" });
-  }
-  
-
   try {
+    if (!req.file) {
+      return res.status(400).json({ error: "No File Uploaded" });
+    }
+    const audioBuffer = req.file.buffer;
+    // console.log(audioBuffer);
+    console.log("Audio file received:", req.file);
+
     res.status(201).json({
       success: "Uploaded Successfully",
       fileName: req.file.originalname,
       fileSize: req.file.size,
     });
+    console.log(upload);
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
--- backend/Upload/audioUpload.js
+++ backend/Upload/audioUpload.js
@@ -1,6 +1,17 @@
 const multer = require("multer");
-const upload = multer({ storage: multer.memoryStorage() });
 
+const audioMimeTypes = ["audio/mp4", "audio/mpeg", "audio/wav", "audio/ogg"];
 
+const fileFilter = (req, file, cb) => {
+  if (audioMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only audio files are allowed"), false);
+  }
+};
+
+const upload = multer({ storage: multer.memoryStorage(), fileFilter });
+
+// console.log(upload)
 
 module.exports = upload;
