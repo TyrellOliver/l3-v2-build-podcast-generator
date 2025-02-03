95fe8529d6ea60022f3a8f901ec3d44236467d75
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-02-03 16:19:11 -0500

    style: Updated the form css to look more like a submission form

--- PodGenFront/src/App.jsx
+++ PodGenFront/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Podcast Generator</h1>
+      <form className="pod_form">
+        <div className="upload-buttons">
+          <button>Upload Audio</button>
+          <br />
+          <button>Enter Transcript</button>
+        </div>
+        <br />
+        <input
+          type="text"
+          id="transcript"
+          name=""
+          placeholder="Paste your text here..."
+        />
+        <br />
+        <button>Generate Podcast</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
